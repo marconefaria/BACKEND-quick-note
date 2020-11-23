db258c37d4f51922773d5c5ace0b5ff325f5979a
Marcone Faria <marconefaria@cpejr.com.br>
2020-11-23 17:23:05 -0300

    criando as migrations

---
+++ src/database/migrations/20201123131603_note.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("note", function(table) {
+        table.string("note_id").primary().notNullable();
+        table.string("user_id").notNullable();
+        table.foreign("user_id").references("user_id").inTable("user").onDelete("cascade");
+        table.string("title").notNullable();
+        table.string("description").notNullable();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable("note");
+  };
+  
---
+++ src/database/migrations/20201123132240_category.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("category", function(table) {
+        table.increments("category_id").primary().notNullable();
+        table.string("user_id").notNullable();
+        table.foreign("user_id").references("user_id").inTable("user").onDelete("cascade");
+        table.string("name").notNullable();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable("category");
+  };
+
+  
